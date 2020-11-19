cf8328de7dd0895781b0338c63a10cba2e45f983
HetviThanki <hetvi.thanki@gmail.com>
2020-11-19 17:51:12 +0530

    My second commit, new class

--- MyJavaProject/src/com/lti/demo/ClientApp.java
+++ MyJavaProject/src/com/lti/demo/ClientApp.java
@@ -4,6 +4,8 @@ public class ClientApp {
 	public static void main(String[] args) {
 		Person p=new Person("Hetvi",22,30000.00);
 		System.out.println(p);
+		Person p1=new Person("Sima",45,30000.00);
+		System.out.println(p1);
 	}
 
 }
